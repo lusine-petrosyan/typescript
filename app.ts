20121a27ef7e796e11618d9364fea04f0038b910
LusinePetrosyan <lusinepetrosyan629@gmail.com>
2016-08-04 17:51:43 +0400

    Singleton pattern

---
+++ app.ts
@@ -0,0 +1,37 @@
+class Singleton
+{
+    public static instance;
+
+    constructor(test)
+    {
+        if (test !== Test)
+        {
+            throw new Error("Error: Instantiation failed. Singleton module! Use .getInstance() instead of new.");
+        }
+    }
+
+    public static getInstance()
+    {
+        if (Singleton.instance == null)
+        {
+            // console.log("create new instance");
+            Singleton.instance = new Singleton(Test);
+        }else
+        {
+            // console.log("error");
+        }
+        return Singleton.instance;
+    }
+}
+
+function Test()
+{
+}
+Singleton.getInstance();
+Singleton.getInstance();
+
+// function Test1()
+// {
+// };
+// let s = new Singleton(Test1); // i's will be error !!
+
